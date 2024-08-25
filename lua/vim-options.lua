280df74671da4888a97be3f29c181118ada1dd78
iaschnei <iaschnei@student.42.fr>
2024-08-25 23:17:00 +0200

    Pushed config to github

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+--- VIM GENERAL CONFIG ---
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
+
+vim.g.mapleader = " "
