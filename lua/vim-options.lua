d567212e5c257b00a53df4b520e0243e9b87611a
iaschnei <iaschnei@student.42.fr>
2024-12-29 17:16:56 +0100

    Added rust support

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+--- VIM GENERAL CONFIG ---
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2 smarttab")
+vim.cmd("set number")
+
+vim.g.mapleader = " "
