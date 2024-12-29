8d041c66be4fde1c74495dc4487027e44a529320
iaschnei <iaschnei@student.42.fr>
2024-12-29 17:16:28 +0100

    Reset

--- lua/vim-options.lua
+++
@@ -1,9 +0,0 @@
---- VIM GENERAL CONFIG ---
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-
-vim.g.mapleader = " "
